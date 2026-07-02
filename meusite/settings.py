43cfa4de7a90fe43ab7ce6b006d3c6ac5f3e7cb8
Rodrigo Marques <164966269+RodrigoLorensiMarques@users.noreply.github.com>
2026-07-02 19:04:45 -0300

    chore: desabilita debug

--- meusite/settings.py
+++ meusite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^zvn^x14=r+l*(^6+zozt(*56!%swbldwoic4xy1_in8^0nq$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
